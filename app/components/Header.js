5dfd82b43febf4aba133c386fb21b17292795845
taniavaldivia <tania.valdiviag@gmail.com>
2022-06-28 21:36:51 -0500

    prueba en header

--- app/components/Header.js
+++ app/components/Header.js
@@ -8,16 +8,27 @@ function Header() {
         <h4 className="my-0 mr-md-auto font-weight-normal">
           <Link to="/" className="text-white">
             {" "}
-            ComplexApp{" "}
+            TestApp{" "}
           </Link>
         </h4>
         <form className="mb-0 pt-2 pt-md-0">
           <div className="row align-items-center">
             <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-              <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+              <input
+                name="username"
+                className="form-control form-control-sm input-dark"
+                type="text"
+                placeholder="Username"
+                autoComplete="off"
+              />
             </div>
             <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-              <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+              <input
+                name="password"
+                className="form-control form-control-sm input-dark"
+                type="password"
+                placeholder="Password"
+              />
             </div>
             <div className="col-md-auto">
               <button className="btn btn-success btn-sm">Sign In</button>
