0ba8b26c4bf42950e5dddcb5a7e94c88797ff2f1
taniavaldivia <tania.valdiviag@gmail.com>
2022-06-30 14:18:11 -0500

    pass props for state2

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom"
 import HeaderLoggedOut from "./HeaderLoggedOut"
 import HeaderLoggedIn from "./HeaderLoggedIn"
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState()
-
+function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -15,11 +13,10 @@ function Header() {
             ComplexApp{" "}
           </Link>
         </h4>
-        {/* Consultar este terniario con Bob, no entiendo al 100 por que le pase prop.Leccion 34 */}
-        {loggedIn ? (
-          <HeaderLoggedIn setLoggedIn={setLoggedIn} />
+        {props.loggedIn ? (
+          <HeaderLoggedIn setLoggedIn={props.setLoggedIn} />
         ) : (
-          <HeaderLoggedOut setLoggedIn={setLoggedIn} />
+          <HeaderLoggedOut setLoggedIn={props.setLoggedIn} />
         )}
       </div>
     </header>
