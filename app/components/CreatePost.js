22302dcf7ca0cd1afb8faca608fe9144686ef844
taniavaldivia <tania.valdiviag@gmail.com>
2022-07-04 16:48:00 -0500

    Context & Use reducer

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import Page from "./Page"
 import Axios from "axios"
-import ExampleContext from "../ExampleContext"
+import DispatchContext from "../DispatchContext"
 
 function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
   const navigate = useNavigate()
-  const { addFlashMessage } = useContext(ExampleContext)
+  const appDispatch = useContext(DispatchContext)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -19,7 +19,10 @@ function CreatePost(props) {
         token: localStorage.getItem("complexappToken"),
       })
       // Redirect to new post url
-      addFlashMessage("Congrats!!!!, you succesfully created a post")
+      appDispatch({
+        type: "flashMessage",
+        value: "Congrats, you created a new post",
+      })
       navigate(`/post/${response.data}`)
       console.log("New post was created.")
     } catch (e) {
